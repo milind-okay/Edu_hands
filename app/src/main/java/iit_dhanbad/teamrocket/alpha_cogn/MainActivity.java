e7e8351ef97c2ad77a2f1b52bab4f874050304f6
milind-okay <milind0359@gmail.com>
2016-09-03 18:24:16 +0530

    other useful files

--- app/src/main/java/iit_dhanbad/teamrocket/alpha_cogn/MainActivity.java
+++ app/src/main/java/iit_dhanbad/teamrocket/alpha_cogn/MainActivity.java
@@ -1,10 +1,20 @@
 package iit_dhanbad.teamrocket.alpha_cogn;
 
+import android.Manifest;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,21 +24,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import iit_dhanbad.NavFunctions;
+import iit_dhanbad.teamrocket.alpha_cogn.utils.Const;
+import iit_dhanbad.teamrocket.alpha_cogn.utils.InternetConnectionDetector;
+import iit_dhanbad.teamrocket.alpha_cogn.utils.PermissionUtils;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener , GoogleMap.OnMyLocationButtonClickListener,
         OnMapReadyCallback, GoogleMap.OnMarkerDragListener,
         ActivityCompat.OnRequestPermissionsResultCallback, View.OnClickListener{
+
+    LatLng mylatlng;
+    Marker myMarker;
+    int radius;
+    Button searchThisArea;
+    double  minlat, maxlat, minlng, maxlng;
+    private ProgressDialog pDialog;
+    boolean moveCamera,isList;
+    InternetConnectionDetector internetConnectionDetector;
+    String url;
+    private HashMap<String, Marker> mPlaceIdAnd = new HashMap<>();
     private static final String MAP_FRAGMENT_TAG = "map";
     /**
      * Request code for location permission request.
@@ -114,41 +160,107 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
-
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        // Log.d("at show MissingPer", "Dialog");
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(false).show(getSupportFragmentManager(), "dialog");
+    }
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
+        NavFunctions navFunctions = new NavFunctions(id, MainActivity.this);
+        navFunctions.selectItem();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+     /*   if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
 
+*/
+        // Log.d("at permissionresult", "ok");
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            enableMyLocation();
+            mPermissionDenied = false;
+        } else {
+
+            mPermissionDenied = true;
+        }
+    }
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        // Log.d("at resume fragment", "ok");
+        if (mPermissionDenied) {
+
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        moveCamera = true;
+        mMap = map;
+        // Log.d("onMapready", "okay--");
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationChangeListener(myLocationChangeListener());
+
+        enableMyLocation();
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener()
+        {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition)
+            {
+                if (!isList) {
+                    double difflat = mMap.getCameraPosition().target.latitude - mylatlng.latitude,maxdifflat = maxlat-minlat;
+                    double difflng = mMap.getCameraPosition().target.longitude - mylatlng.longitude,maxdifflng = maxlng-minlng;
+                    if(difflat<0.0)
+                        difflat=-difflat;
+                    if(difflng<0.0)
+                        difflng=-difflng;
+
+                    if ((difflat) > (maxdifflat) || (difflng) > (maxdifflng))
+                        // if ((mMap.getCameraPosition().target.latitude) > maxlat  ||  mMap.getCameraPosition().target.longitude >  maxlng
+                        //       ||mMap.getCameraPosition().target.latitude > minlat ||  mMap.getCameraPosition().target.longitude >  minlng)
+                        searchThisArea.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+            // PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE, Manifest.permission.ACCESS_COARSE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+
+
+        }
+    }
     @Override
     public void onClick(View v) {
 
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-    }
 
     @Override
     public void onMarkerDragStart(Marker marker) {
@@ -164,10 +276,236 @@ public class MainActivity extends AppCompatActivity
     public void onMarkerDragEnd(Marker marker) {
 
     }
-
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, "MAp Not ready yet", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+    /**
+     * Called when the Clear button is clicked.
+     */
+    public void onClearMap(View view) {
+        if (!checkReady()) {
+            return;
+        }
+        mMap.clear();
+    }
+    /**
+     * Called when the Reset button is clicked.
+     */
+    public void onResetMap() {
+        if (!checkReady()) {
+            return;
+        } else {
+            mMap.clear();
+        }
+    }
     @Override
     public boolean onMyLocationButtonClick() {
         return false;
     }
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener() {
+        return new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+
+                if (moveCamera) {
+                    mylatlng = new LatLng(location.getLatitude(), location.getLongitude());
+
+
+                    //  Log.d("at movecamera", "true");
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mylatlng, 15.0f), new GoogleMap.CancelableCallback() {
+                        @Override
+                        public void onFinish() {
+                            setApi();
+                        }
+
+                        @Override
+                        public void onCancel() {
+
+                        }
+                    });
+                    myMarker = mMap.addMarker(new MarkerOptions()
+                            .position(mylatlng)
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker))
+                            .title("Your location")
+                            .snippet("you can drag it")
+                            .draggable(true));
+                    moveCamera = false;
+
+                }
+            }
+            //Toast.makeText(getApplicationContext(),"You are at [" + longitude + " ; " + latitude + " ]",Toast.LENGTH_LONG).show();
+
+            //get current address by invoke an AsyncTask object
+
+
+        };
+    }
+/********************end map section*********************************************************/
+    private void sendRequest() {
+        //  Log.d("at ", "sendRequest");
+        internetConnectionDetector = new InternetConnectionDetector(getApplicationContext());
+        if (internetConnectionDetector.isConnectingToInternet()) {
+            Log.d("sending request", url);
+            //pDialog.setTitle("");
+            searchThisArea.setVisibility(View.GONE);
+            if (pDialog == null) {
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Fetching Clinics around You");
+                pDialog.setCancelable(true);
+                pDialog.show();
+            }
+            StringRequest stringRequest = new StringRequest(url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            try {
+                                JSONObject jsonObject= new JSONObject(response);
+                                if(jsonObject.getString("status").equals("OK")){
+                                    if (isList) {/**TODO check listview
+                                        ListView fragment = (ListView) getFragmentManager().findFragmentById(R.id.list);
+                                        if (fragment != null) {
+                                            fragment.showPlacesList(response);
+                                        }*/
+                                    }/* else if (isAds)
+                                try {
+                                    parseAds(response);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }*/ else
+                                        showPlaces(response);
+                                }else{
+                                    Toast.makeText(getApplicationContext(),"No Clinic Found in 100 Km radius",Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            pDialog.dismiss();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        } else {
+            Toast.makeText(this, "No Internet connection", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void showPlaces(String result) {
+        onResetMap();
+
+        myMarker = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(mylatlng.latitude, mylatlng.longitude))
+                .title("Your location")
+                .snippet("you can drag it")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker))
+                .draggable(true));
+        ArrayList<String> temp = new ArrayList<>();
+        temp.add("m");
+        temp.add("!");
+        MapPlacesDisplayTask placesDisplayTask = new MapPlacesDisplayTask();
+        Object[] toPass = new Object[2];
+        toPass[0] = mMap;
+        toPass[1] = result;
+        placesDisplayTask.execute(toPass);
+
+    }
+
+    public void setApi() {
+        url = "";
+        sendRequest();
+
+    }
+    public class MapPlacesDisplayTask extends AsyncTask<Object, Integer, List<HashMap<String, String>>> {
+
+        JSONObject googlePlacesJson;
+        GoogleMap googleMap;
+
+        @Override
+        protected List<HashMap<String, String>> doInBackground(Object... inputObj) {
+
+            List<HashMap<String, String>> googlePlacesList = null;
+            Places placeJsonParser = new Places();
+            try {
+                googleMap = (GoogleMap) inputObj[0];
+                googlePlacesJson = new JSONObject((String) inputObj[1]);
+                googlePlacesList = placeJsonParser.parse(googlePlacesJson);
+            } catch (Exception e) {
+                //    Log.d("Exception", e.toString());
+            }
+            return googlePlacesList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> list) {
+            //  googleMap.clear();
+
+            minlat = mylatlng.latitude;
+            minlng = mylatlng.longitude;
+            maxlat = mylatlng.latitude;
+            maxlng = mylatlng.longitude;
+
+            // mMarkerHash.clear();
+            if (list != null)
+                for (int i = 0; i < list.size(); i++) {
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    HashMap<String, String> googlePlace = list.get(i);
+                    double lat = Double.parseDouble(googlePlace.get("lat"));
+                    double lng = Double.parseDouble(googlePlace.get("lng"));
+                    String placeName = googlePlace.get("place_name");
+                    String vicinity = googlePlace.get("vicinity");
+                    LatLng latLng = new LatLng(lat, lng);
+                    markerOptions.position(latLng);
+                    markerOptions.title(placeName);
+                    markerOptions.snippet(vicinity);
+                    ArrayList<String> temp = new ArrayList<>();
+                    temp.add(googlePlace.get("reference"));
+                    Marker marker;
+                    /*if (googlePlace.get("type").equals(Const.HC_RESULT_TYPE1)) {
+                        temp.add(googlePlace.get("ad"));
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+                        marker = mMap.addMarker(markerOptions);
+                    } else if (googlePlace.get("type").equals(Const.HC_RESULT_TYPE2)) {
+                        temp.add("!");
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                        marker = mMap.addMarker(markerOptions);
+                        blueMarkers.put(marker, googlePlace.get("hcid"));
+                    } else {
+                        temp.add("!");
+                        marker = mMap.addMarker(markerOptions);
+                    }
+*/                  marker = mMap.addMarker(markerOptions);
+                    mPlaceIdAnd.put(googlePlace.get("reference"), marker);
+                    if (minlat > lat)
+                        minlat = lat;
+                    if (maxlat < lat)
+                        maxlat = lat;
+                    if (minlng > lng)
+                        minlng = lng;
+                    if (maxlng < lng)
+                        maxlng = lng;
+
+                }
+            LatLngBounds bounds = new LatLngBounds.Builder()
+                    .include(new LatLng(minlat, minlng))
+                    .include(new LatLng(maxlat, maxlng))
+                    .build();
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+            // pDialog.dismiss();
+        }
+
+    }
+
 
 }
---
+++ app/src/main/java/iit_dhanbad/teamrocket/alpha_cogn/Places.java
@@ -0,0 +1,75 @@
+package iit_dhanbad.teamrocket.alpha_cogn;
+
+/**
+ * Created by milind on 3/9/16.
+ */
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Places {
+
+    public List<HashMap<String, String>> parse(JSONObject jsonObject) {
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = jsonObject.getJSONArray("results");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return getPlaces(jsonArray);
+    }
+
+    private List<HashMap<String, String>> getPlaces(JSONArray jsonArray) {
+        int placesCount = jsonArray.length();
+        List<HashMap<String, String>> placesList = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> placeMap = null;
+
+        for (int i = 0; i < placesCount; i++) {
+            try {
+                placeMap = getPlace((JSONObject) jsonArray.get(i));
+                placesList.add(placeMap);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return placesList;
+    }
+
+    private HashMap<String, String> getPlace(JSONObject googlePlaceJson) {
+        HashMap<String, String> googlePlaceMap = new HashMap<String, String>();
+        String placeName = "-NA-";
+        String vicinity = "-NA-";
+        String latitude = "";
+        String longitude = "";
+        String reference = "";
+
+        try {
+            if (!googlePlaceJson.isNull("name")) {
+                placeName = googlePlaceJson.getString("name");
+            }
+            if (!googlePlaceJson.isNull("vicinity")) {
+                vicinity = googlePlaceJson.getString("vicinity");
+            }
+            latitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lat");
+            longitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lng");
+            reference = googlePlaceJson.getString("place_id");
+            googlePlaceMap.put("place_name", placeName);
+            googlePlaceMap.put("vicinity", vicinity);
+            googlePlaceMap.put("lat", latitude);
+            googlePlaceMap.put("lng", longitude);
+            googlePlaceMap.put("reference", reference);
+            googlePlaceMap.put("type", googlePlaceJson.getString("type"));
+            googlePlaceMap.put("hcid", googlePlaceJson.getString("hcid"));
+            googlePlaceMap.put("ad", googlePlaceJson.getString("ad"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return googlePlaceMap;
+    }
+}
