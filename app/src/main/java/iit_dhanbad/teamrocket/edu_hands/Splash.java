e914945e8f148496c11a146b3eb1a32758b936a5
milind-okay <milind0359@gmail.com>
2016-09-03 12:49:28 +0530

    first commit

---
+++ app/src/main/java/iit_dhanbad/teamrocket/edu_hands/Splash.java
@@ -0,0 +1,13 @@
+package iit_dhanbad.teamrocket.edu_hands;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
